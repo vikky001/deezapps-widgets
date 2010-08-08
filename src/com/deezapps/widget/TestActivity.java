5f6908b61a802560cfbcdbc281d78a4fb261814e
jeanguy@gmail.com <jeanguy@gmail.com@4ff8b62d-5845-2e33-a3dd-119b659cf731>
2010-08-08 22:05:04 +0000

    Initial implementation of HorizontalPager with test activity

---
+++ src/com/deezapps/widget/TestActivity.java
@@ -0,0 +1,46 @@
+package com.deezapps.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * User: jean-guyspeton
+ * Date: Aug 8, 2010
+ */
+public class TestActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        final HorizontalPager pager = (HorizontalPager) findViewById(R.id.pager);
+        pager.addOnScrollListener(new HorizontalPager.OnScrollListener() {
+            public void onScroll(int scrollX) {
+                Log.d("TestActivity", "scrollX=" + scrollX);
+            }
+
+            public void onViewScrollFinished(int viewIndex) {
+                Log.d("TestActivity", "viewIndex=" + viewIndex);
+            }
+        });
+
+        Button b = (Button) findViewById(R.id.left);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                pager.scrollLeft();
+            }
+        });
+        b = (Button) findViewById(R.id.right);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                pager.scrollRight();
+            }
+        });
+    }
+}
